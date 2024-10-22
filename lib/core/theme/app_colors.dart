39b8b5353dfc2f404101a4ffbd97ddc113f1eb2d
devmoaid1 <moaidmohamed1312@gmail.com>
2024-10-22 20:58:16 +0300

    added app colors

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,1 +1,11 @@
-abstract class AppColors {}
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primaryColor = Color(0xFF00A0B0);
+  static const Color secondaryColor = Color(0xFF7C8183);
+  static const Color darkGray = Color(0xFF3F4345);
+  static const Color borderGray = Color(0xFF7C8183);
+  static const Color errorColor = Color(0xFFEF5350);
+}
--- lib/core/theme/light_theme.dart
+++ lib/core/theme/light_theme.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_payment/core/theme/app_colors.dart';
 import 'package:flutter_payment/core/theme/text_theme.dart';
 
 final ThemeData lightTheme = ThemeData(
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    colorScheme: ColorScheme.fromSeed(
+        primary: AppColors.primaryColor,
+        secondary: AppColors.secondaryColor,
+        error: AppColors.errorColor,
+        shadow: AppColors.borderGray,
+        outline: AppColors.darkGray,
+        seedColor: AppColors.primaryColor),
+    scaffoldBackgroundColor: Colors.white,
     useMaterial3: true,
     textTheme: customTextTheme);
