0af6261a25eace3d1906993c212c74ace1885cb1
devmoaid1 <moaidmohamed1312@gmail.com>
2024-10-22 19:57:20 +0300

    tested app and works fine

--- lib/flutter_payment.dart
+++ lib/flutter_payment.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_payment/core/theme/light_theme.dart';
 import 'package:flutter_payment/presentation/checkout/views/checkout_view.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FlutterPaymentApp extends StatelessWidget {
+  const FlutterPaymentApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Payment',
       theme: lightTheme,
       home: const CheckoutView(),
     );
