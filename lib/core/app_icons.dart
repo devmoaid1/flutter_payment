5741341abdb28f776cc575289a14852447d6c2b2
devmoaid1 <moaidmohamed1312@gmail.com>
2024-10-22 20:18:17 +0300

    added assets

---
+++ lib/core/app_icons.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsIconsDelete
+  /// assets/icons/delete.svg
+  static const String assetsIconsDelete = "assets/icons/delete.svg";
+
+  /// Assets for assetsIconsMastercard
+  /// assets/icons/Mastercard.svg
+  static const String assetsIconsMastercard = "assets/icons/Mastercard.svg";
+
+  /// Assets for assetsIconsPaypal
+  /// assets/icons/paypal.svg
+  static const String assetsIconsPaypal = "assets/icons/paypal.svg";
+
+  /// Assets for assetsIconsProduct
+  /// assets/icons/product.png
+  static const String assetsIconsProduct = "assets/icons/product.png";
+
+  /// Assets for assetsIconsSuccess
+  /// assets/icons/success.svg
+  static const String assetsIconsSuccess = "assets/icons/success.svg";
+
+  /// Assets for assetsIconsVisa
+  /// assets/icons/visa.svg
+  static const String assetsIconsVisa = "assets/icons/visa.svg";
+}
+
